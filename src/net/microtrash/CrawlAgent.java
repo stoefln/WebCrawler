c634aa26995f1a74c36e3e5503368b064fa33ee6
Stephan Petzl <stephan.petzl@ajado.com>
2012-01-11 22:43:58 +0100

    basic communication functional

--- src/net/microtrash/CrawlAgent.java
+++ src/net/microtrash/CrawlAgent.java
@@ -1,18 +1,89 @@
 package net.microtrash;
 
+import java.util.EmptyStackException;
+
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
 
 
 public class CrawlAgent extends Agent {
 
-	private void sendMessage(){
-		ACLMessage msg = new ACLMessage(ACLMessage.INFORM); 
-		msg.addReceiver(new AID("Peter", AID.ISLOCALNAME)); 
-		msg.setLanguage("English"); 
-		msg.setOntology("Weather-forecast-ontology"); 
-		msg.setContent("Today it’s raining");
-		send(msg);
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	protected void log(String message) {
+		System.out.println(message);
+	}
+	
+	@Override
+	protected void setup() {
+		DFAgentDescription agentDescription = new DFAgentDescription(); 
+		agentDescription.setName(getAID()); 
+		ServiceDescription serviceDescription = new ServiceDescription(); 
+		serviceDescription.setType("crawling"); 
+		serviceDescription.setName("webpage crawler"); 
+		agentDescription.addServices(serviceDescription);
+		try { 
+			DFService.register(this, agentDescription);
+		} catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+		addBehaviour(new ParseRequestReceiver());
+		
+		super.setup();
+	}
+
+	protected void takeDown() { // Deregister from the yellow pages try {
+		try {
+			DFService.deregister(this); 
+		} catch (FIPAException fe) {
+			fe.printStackTrace();
+		}
+
+		System.out.println("CrawlAgent "+getAID().getName()+" sais good bye");
+	}
+
+	private class ParseRequestReceiver extends CyclicBehaviour{
+		private static final long serialVersionUID = 1L;
+
+		@Override
+		public void action() {
+			log("ParseRequestReceiver action()");
+			ACLMessage request = myAgent.receive(); 
+			if (request != null) {
+				log("got message!");
+				String url = request.getContent();
+			
+				// TODO: send HTTP request, parse response
+				String urls = "http://www.github.com,http://www.doodle.com";
+				log("returning message...");
+				
+				// TODO: should we move following code to an extra Behavior?
+				ACLMessage reply = request.createReply();
+				 
+				if (urls != null) { // if there is a valid response data object
+					reply.setPerformative(ACLMessage.PROPOSE);
+					reply.setContent(urls);
+				} else {
+					reply.setPerformative(ACLMessage.REFUSE); 
+					reply.setContent("404 - not found!");
+				} 
+				myAgent.send(reply);
+				
+			} else {
+				log("ParseResponseReceiver block()");
+				block();
+			}
+			
+		}
+		
 	}
 }
--- src/net/microtrash/DatabaseAgent.java
+++ src/net/microtrash/DatabaseAgent.java
@@ -2,14 +2,18 @@ package net.microtrash;
 
 import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.CyclicBehaviour;
 import jade.core.behaviours.TickerBehaviour;
-
 import jade.domain.DFService;
 import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
 
+import java.util.EmptyStackException;
 import java.util.Hashtable;
+import java.util.Stack;
 import java.util.Vector;
 
 public class DatabaseAgent extends Agent {
@@ -19,21 +23,26 @@ public class DatabaseAgent extends Agent {
 	 */
 	private static final long serialVersionUID = 2L;
 
-	private Hashtable<String,Webpage> db = new Hashtable<String,Webpage>();
+	private Hashtable<String,Webpage> parsedWebPages = new Hashtable<String,Webpage>();
+	private Stack<String> unparsedLinks = new Stack<String>();
 	private Vector<AID> crawlers = new Vector<AID>();
 	private String seedUrl;
 	
+	protected void log(String message) {
+		System.out.println(message);
+	}
 	@Override
 	protected void setup() {
 		System.out.println("Hallo I'm the DatabaseAgent! My name is "+getAID().getName());
 		
 		Object[] args = getArguments(); 
 		if (args != null && args.length > 0) {
-			seedUrl = (String) args[0]; 
-			System.out.println("Starting with seedUrl "+seedUrl);
+			seedUrl = (String) args[0];
+			unparsedLinks.push(seedUrl);
+			log("Starting with seedUrl "+seedUrl);
 		}else{
 			// Make the agent terminate immediately
-			System.out.println("No seedUrl specified"); 
+			log("No seedUrl specified"); 
 			doDelete();
 			return;
 		}
@@ -44,17 +53,18 @@ public class DatabaseAgent extends Agent {
 			private static final long serialVersionUID = 1L;
 
 			protected void onTick() {
-		
-				// Update the list of seller agents
+				log("Update the list of crawler agents.");
 				DFAgentDescription template = new DFAgentDescription(); 
 				ServiceDescription sd = new ServiceDescription(); 
 				sd.setType("crawling"); 
 				template.addServices(sd);
-				try { 
+				crawlers = new Vector<AID>();
+				try {
+					log("All agents: ");
 					DFAgentDescription[] result = DFService.search(myAgent, template); 
-
 					for (int i = 0; i < result.length; ++i) {
-						registerCrawlAgent(result[i].getName());
+						crawlers.add(result[i].getName());
+						log("  "+result[i].getName());
 					}
 				}catch(FIPAException fe) {
 					fe.printStackTrace();
@@ -65,6 +75,10 @@ public class DatabaseAgent extends Agent {
 		
 		// 2) loop through all available crawlers and push a linkUrl to each of them (as long as there are links to other webpages which are not present in the DB by now) 
 		addBehaviour(new ParseRequestPerformer());
+		
+		// 3) get responses, insert page into the db and add unparsed urls to the unparsedUrls Stack
+		addBehaviour(new ParseResponseReceiver());
+		
 		super.setup();
 	}
 	
@@ -74,17 +88,63 @@ public class DatabaseAgent extends Agent {
 	}
 	
 	public void addWebpage(Webpage page) {
-		db.put(page.getUrl(),page);
+		parsedWebPages.put(page.getUrl(),page);
 	}
 	
 	public boolean isWebpageParsed(String pageUrl){
-		return db.contains(pageUrl);
+		return parsedWebPages.contains(pageUrl);
 	}
 	
-	protected void registerCrawlAgent(AID aid){
-		if(crawlers.indexOf(aid) != -1){
-			crawlers.add(aid);
+	private class ParseRequestPerformer extends CyclicBehaviour{
+		
+		private static final long serialVersionUID = 1L;
+
+		
+		@Override
+		public void action() {
+			log("ParseRequestPerformer action()");
+			try{
+				for(AID aid : crawlers ){
+					String link = unparsedLinks.pop();
+					log("send link for parsing: "+link);
+					ACLMessage message = new ACLMessage(ACLMessage.CFP);
+					message.addReceiver(aid);
+					message.setContent(link);
+					message.setConversationId("parse-url");
+					message.setReplyWith("message_"+aid+"_"+System.currentTimeMillis());
+					myAgent.send(message);
+				}
+			}catch(EmptyStackException e){} 
+			log("ParseRequestPerformer block()");
+			
+			block();
 		}
+
+		
 	}
-	
+
+	private class ParseResponseReceiver extends CyclicBehaviour{
+		
+		private static final long serialVersionUID = 2L;
+
+		@Override
+		public void action() {
+			log("ParseResponseReceiver action()");
+			ACLMessage reply = myAgent.receive(); 
+			if (reply != null) {
+				String urls = reply.getContent();
+				String[] urlArr = urls.split(",");
+				for(String url : urlArr){
+					System.out.println(url);
+				}
+			} else {
+				log("ParseResponseReceiver block()");
+				block();
+			}
+
+		}
+
+		
+	}
 }
+
