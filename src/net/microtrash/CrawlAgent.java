1a4f42d6eb81d8f055a6cfd7f322bda36abf04e0
Thomas Haider <t.haider@deprecate.de>
2012-01-12 18:15:23 +0100

    Added URL retrival code to Crawler

--- src/net/microtrash/CrawlAgent.java
+++ src/net/microtrash/CrawlAgent.java
@@ -1,8 +1,5 @@
 package net.microtrash;
 
-import java.util.EmptyStackException;
-
-import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.domain.DFService;
@@ -10,6 +7,15 @@ import jade.domain.FIPAException;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.lang.acl.ACLMessage;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 
 public class CrawlAgent extends Agent {
@@ -53,6 +59,14 @@ public class CrawlAgent extends Agent {
 
 	private class ParseRequestReceiver extends CyclicBehaviour{
 		private static final long serialVersionUID = 1L;
+		
+		void reportFailure(ACLMessage request, String text) {
+			log("reporting back: " + text);
+			ACLMessage reply = request.createReply();
+			reply.setPerformative(ACLMessage.REFUSE);
+			reply.setContent(text);
+			myAgent.send(reply);
+		}
 
 		@Override
 		public void action() {
@@ -63,20 +77,50 @@ public class CrawlAgent extends Agent {
 				String url = request.getContent();
 			
 				// TODO: send HTTP request, parse response
-				String urls = "http://www.github.com,http://www.doodle.com";
-				log("returning message...");
-				
-				// TODO: should we move following code to an extra Behavior?
-				ACLMessage reply = request.createReply();
-				 
-				if (urls != null) { // if there is a valid response data object
-					reply.setPerformative(ACLMessage.PROPOSE);
-					reply.setContent(urls);
-				} else {
-					reply.setPerformative(ACLMessage.REFUSE); 
-					reply.setContent("404 - not found!");
-				} 
-				myAgent.send(reply);
+				// TODO: robots.txt?
+				URL u;
+				URLConnection urlConnection = null;
+				try {
+					u = new URL(url);
+					urlConnection = u.openConnection();
+					urlConnection.setAllowUserInteraction(false);
+					InputStream stream = urlConnection.getInputStream();
+					BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+					String type = urlConnection.getContentType();
+					log("Content Type: " + type);
+					if(!type.contains("text/html")) {
+						throw new UnsupportedEncodingException("Not a html page");
+					}
+					String line;
+					StringBuilder content = new StringBuilder();
+					while((line = reader.readLine()) != null) {
+						content.append(line);
+					}
+					reader.close();
+					// TODO: Parse content and extract URLs
+
+					// TODO: "," is not a good seperator, can be part of URLs 
+					// (ref: http://stackoverflow.com/questions/1547899/which-characters-make-a-url-invalid)
+					String urls = "http://www.github.com,http://www.doodle.com";
+					log("returning message...");
+					
+					// TODO: should we move following code to an extra Behavior?
+					ACLMessage reply = request.createReply();
+					 
+					if (urls != null) { // if there is a valid response data object
+						reply.setPerformative(ACLMessage.PROPOSE);
+						reply.setContent(urls);
+					} else {
+						reportFailure(request, "No links found");
+					} 
+					myAgent.send(reply);
+				} catch (MalformedURLException e) {
+					reportFailure(request, "MalformedURLException");
+				} catch (UnsupportedEncodingException e) {
+					reportFailure(request, e.getMessage());
+				} catch (IOException e) {
+					reportFailure(request, "Connection Error");
+				}
 				
 			} else {
 				log("ParseResponseReceiver block()");
